7892dd218afd71f7545c95c312af332a748ec4d4
Veronica Andersen <v.andersen98@gmail.com>
2025-03-18 09:58:41 +0100

    Fixed styling on form and buttons

--- src/Components/BookingPage.js
+++ src/Components/BookingPage.js
@@ -5,10 +5,10 @@ export default function BookingPage ({availableTimes, dispatch}) {
 
 
     return (
-        <>
+        <div className="booking-page">
             <h1 className='section-title'>Reserve a Table</h1>
             <p>Book your table in advance to enjoy a wonderful dining experience!</p>
             <BookingForm availableTimes={availableTimes} dispatch={dispatch} />
-        </>
+        </div>
     );
 };
